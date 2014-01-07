2e7383395a2954e53a77dc98cff9a67d07f6c69c
slugmee <mee@simon.idv.hk>
2014-01-07 12:42:33 +0800

    Update Get_TextElement_From_String.js

--- Get_TextElement_From_String.js
+++ Get_TextElement_From_String.js
@@ -1,2 +1,11 @@
-// init
-// dev branch test
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+</head>
+<body>
+
+<p contenteditable="true">This is a paragraph. It is editable. Try to change this text.</p>
+
+</body>
+</html>
